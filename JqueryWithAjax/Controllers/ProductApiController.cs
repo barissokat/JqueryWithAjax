8218c7c6a21198de1d7867c8c6ea471ced0ce448
bsokat <barissokat@gmail.com>
2017-03-06 09:14:36 +0300

    Fix web api routing for all ajax examples

---
+++ JqueryWithAjax/Controllers/ProductApiController.cs
@@ -0,0 +1,20 @@
+﻿using JqueryWithAjax.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JqueryWithAjax.Controllers
+{
+    public class ProductApiController : ApiController
+    {
+        [HttpGet]
+        public Product Product()
+        {
+            var product = (from p in Data.Products where p.ID == 1 select p).FirstOrDefault();
+            return product;
+        }
+    }
+}
