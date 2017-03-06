622bff10c85a2eb9d1602cb453e7adf50a9497c3
bsokat <barissokat@gmail.com>
2017-03-06 09:03:34 +0300

    Model classes added

---
+++ JqueryWithAjax/Models/Data.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JqueryWithAjax.Models
+{
+    public class Data
+    {
+        private static List<Product> _products;
+        public static List<Product> Products
+        {
+            get
+            {
+                if (_products == null)
+                {
+                    _products = new List<Product>
+                    {
+                        new Product
+                        {
+                            ID = 1, Name = "PC", Price = 1200
+                        },
+                        new Product
+                        {
+                            ID = 1, Name = "Notebook", Price = 2200
+                        },
+                        new Product
+                        {
+                            ID = 1, Name = "Tablet PC", Price = 3200
+                        },
+                        new Product
+                        {
+                            ID = 1, Name = "Netbook", Price = 4200
+                        },
+                        new Product
+                        {
+                            ID = 1, Name = "Ultrabook", Price = 5200
+                        }
+                    };
+                }
+                return _products;
+            }
+        }
+    }
+}
